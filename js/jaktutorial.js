74c496da6af437b0a7bff0c7d6fa0effa480b81b
derwichmed <43132747+derwichmed@users.noreply.github.com>
2018-09-22 00:16:41 +0200

    Add files via upload

---
+++ js/jaktutorial.js
@@ -0,0 +1,27 @@
+/* Password strength indicator */
+function passwordStrength(password) {
+
+	var desc = [{'width':'0px'}, {'width':'20%'}, {'width':'40%'}, {'width':'60%'}, {'width':'80%'}, {'width':'100%'}];
+	
+	var descClass = ['', 'progress-bar-danger', 'progress-bar-danger', 'progress-bar-warning', 'progress-bar-success', 'progress-bar-success'];
+
+	var score = 0;
+
+	//if password bigger than 6 give 1 point
+	if (password.length > 6) score++;
+
+	//if password has both lower and uppercase characters give 1 point	
+	if ((password.match(/[a-z]/)) && (password.match(/[A-Z]/))) score++;
+
+	//if password has at least one number give 1 point
+	if (password.match(/\d+/)) score++;
+
+	//if password has at least one special caracther give 1 point
+	if ( password.match(/.[!,@,#,$,%,^,&,*,?,_,~,-,(,)]/) )	score++;
+
+	//if password bigger than 12 give another 1 point
+	if (password.length > 10) score++;
+	
+	// display indicator
+	$("#jak_pstrength").removeClass(descClass[score-1]).addClass(descClass[score]).css(desc[score]);
+}
